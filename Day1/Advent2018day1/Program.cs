3bfbe35e53c07cb0f3f0b6c472a9f18a717db386
Rickard Fors <rickard.frs@gmail.com>
2018-12-21 00:25:22 +0100

    Day1

---
+++ Day1/Advent2018day1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent2018day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] frequencys = System.IO.File.ReadAllLines(@"C:\Users\rwfrs\Documents\Projects\Advent2018input\input1.txt");
+
+            int currentFrequency = 0;
+            bool partTwo= false;
+            bool partOne = true;
+            List<int> frequencyList = new List<int>();
+            frequencyList.Add(0);
+
+            while(partTwo == false)
+            { 
+                foreach (string inputFrequency in frequencys)
+                {
+                    int frequency = Convert.ToInt32(inputFrequency);
+                    currentFrequency += frequency;
+
+                    foreach (var number in frequencyList)
+                    {
+                        if (number == currentFrequency && partTwo == false)
+                        {
+                            Console.WriteLine("--- Part Two --- puzzle answer is: " + currentFrequency);
+                            Console.ReadLine();
+                            partTwo = true;
+                        }
+
+                    }
+                    frequencyList.Add(currentFrequency);
+                }
+
+                if (partOne == true)
+                {
+                    Console.WriteLine("--- Part One --- puzzle answer is: " + currentFrequency);
+                    Console.ReadLine();
+                    Console.WriteLine("Looping frequencys...");
+                    partOne = false;
+                }
+                
+            }
+
+
+
+        }
+
+    }
+}
